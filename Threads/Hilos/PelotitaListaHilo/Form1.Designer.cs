ce450427cb74859b514507ca0dcb5733c040a5af
unknown <alumno@LAB02PC13.labs.extension>
2016-11-16 11:49:15 -0300

    Falta hacer lista de pelota-hilo.

---
+++ Threads/Hilos/PelotitaListaHilo/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PelotitaListaHilo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnPausar = new System.Windows.Forms.Button();
+            this.btnReanudar = new System.Windows.Forms.Button();
+            this.btnDestruir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(576, 233);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(13, 267);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(90, 23);
+            this.btnCrear.TabIndex = 1;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnPausar
+            // 
+            this.btnPausar.Location = new System.Drawing.Point(126, 267);
+            this.btnPausar.Name = "btnPausar";
+            this.btnPausar.Size = new System.Drawing.Size(101, 23);
+            this.btnPausar.TabIndex = 2;
+            this.btnPausar.Text = "Pausar";
+            this.btnPausar.UseVisualStyleBackColor = true;
+            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
+            // 
+            // btnReanudar
+            // 
+            this.btnReanudar.Location = new System.Drawing.Point(253, 266);
+            this.btnReanudar.Name = "btnReanudar";
+            this.btnReanudar.Size = new System.Drawing.Size(101, 23);
+            this.btnReanudar.TabIndex = 3;
+            this.btnReanudar.Text = "Reanudar";
+            this.btnReanudar.UseVisualStyleBackColor = true;
+            this.btnReanudar.Click += new System.EventHandler(this.btnReanudar_Click);
+            // 
+            // btnDestruir
+            // 
+            this.btnDestruir.Location = new System.Drawing.Point(450, 266);
+            this.btnDestruir.Name = "btnDestruir";
+            this.btnDestruir.Size = new System.Drawing.Size(138, 51);
+            this.btnDestruir.TabIndex = 4;
+            this.btnDestruir.Text = "destruir";
+            this.btnDestruir.UseVisualStyleBackColor = true;
+            this.btnDestruir.Click += new System.EventHandler(this.btnDestruir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 319);
+            this.Controls.Add(this.btnDestruir);
+            this.Controls.Add(this.btnReanudar);
+            this.Controls.Add(this.btnPausar);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnPausar;
+        private System.Windows.Forms.Button btnReanudar;
+        private System.Windows.Forms.Button btnDestruir;
+    }
+}
+
---
+++ Threads/Hilos/PelotitaListaHilo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PelotitaListaHilo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDestruir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReanudar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPausar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
