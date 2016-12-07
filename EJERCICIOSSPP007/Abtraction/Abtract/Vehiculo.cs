fd91d0925bf70f701e74bec8356bfcc0f0643f9d
laloo96 <ohmycaio@gmail.com>
2016-12-06 22:48:19 -0300

    Update Vehiculo.cs

--- EJERCICIOSSPP007/Abtraction/Abtract/Vehiculo.cs
+++ EJERCICIOSSPP007/Abtraction/Abtract/Vehiculo.cs
@@ -14,7 +14,75 @@ namespace Abtract
 {
     public abstract class Vehiculo
     {
+        
+            public class Deposito
+    {
+        public List<Producto> Lista;
+
+        public Deposito()
+        {
+            this.Lista = new List<Producto>();
+        }
+
+        public static List<Producto> operator +(Deposito uno, Deposito dos)
+        {
+            List<Producto> lista = new List<Producto>();
+            bool estaEnLaLista = false;
+            
+            foreach (Producto itemUno in uno.Lista)
+            {
+                lista.Add(itemUno);
+            }
+            foreach (Producto itemDos in dos.Lista)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (itemDos == lista[i])
+                    {
+                        lista[i].Stock += itemDos.Stock;
+                        estaEnLaLista = true;
+                    }
+                }
+                if (!estaEnLaLista)
+                {
+                    lista.Add(itemDos);
+                }
+            }
+
+            return lista;
+        
         public string patente;
+            
+                public class Galpon<T> : IGuardarXML
+    {
+        public List<T> lista;
+        private int _cantidad;
+
+        public int Cantidad
+        {
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("El valor a ingresar fue 0");
+                }
+                else if (value.EsImpar())
+                {
+                    this.EsImpar(value, new EventArgs());
+                }
+                this._cantidad = value;
+            }
+        }
+
+        public event DelegadoCantidad EsImpar;
+
+        public bool SerializarXML()
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(Galpon<Deposito>));
+
+
+            return false;
+        }
 
         public Vehiculo(string patente)
         {
@@ -35,5 +103,18 @@ namespace Abtract
         public abstract float CalcularCosto();
 
     }
+    
+     public class ClasePila
+    {
+        
+        public static Stack<Double> OrdenInverso(Stack<Double> pilaOriginal)
+        {
+            Stack<Double> pilaNueva = new Stack<double>();          
+            while(pilaOriginal.Count >= 1)
+            {
+                pilaNueva.Push(pilaOriginal.Pop());
+            }
+            return pilaNueva;
+        }
 
 }
